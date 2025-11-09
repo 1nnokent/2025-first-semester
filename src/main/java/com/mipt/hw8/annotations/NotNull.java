26a03b70e8f964db23d5a402912d8e98e651024a
1nnokent <maxlynch2007@gmail.com>
2025-11-09 21:04:32 +0300

    Изменил annotation NotNull

--- src/main/java/com/mipt/hw8/annotations/NotNull.java
+++ src/main/java/com/mipt/hw8/annotations/NotNull.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface NotNull {
-  String message();
+  String message() default "Field cannot be null";
 }
