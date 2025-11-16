f9c3911f4a230b6a3cce1c61aabf32bc9d0f7da1
1nnokent <maxlynch2007@gmail.com>
2025-11-16 18:54:14 +0300

    Создал базовый класс SimpleDataService

---
+++ src/main/java/com/mipt/hw11/SimpleDataService.java
@@ -0,0 +1,36 @@
+package com.mipt.hw11;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+
+interface DataService {
+
+  Optional<String> findDataByKey(String key);
+
+  void saveData(String key, String data);
+
+  boolean deleteData(String key);
+}
+
+
+class SimpleDataService implements DataService {
+
+  private Map<String, String> storage = new HashMap<>();
+
+  @Override
+  public Optional<String> findDataByKey(String key) {
+    return Optional.of(storage.get(key));
+  }
+
+  @Override
+  public void saveData(String key, String data) {
+    storage.put(key, data);
+  }
+
+  @Override
+  public boolean deleteData(String key) {
+    return storage.remove(key) != null;
+  }
+}
